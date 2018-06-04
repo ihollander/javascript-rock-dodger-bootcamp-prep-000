49d8152fdb9a54c4b28f72008b7af4b7a016c769
Ian Hollander <ianholla@gmail.com>
2018-06-04 22:31:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -129,7 +129,7 @@ function createRock(x) {
  * Finally, alert "YOU LOSE!" to the player.
  */
 function endGame() {
-  gameInterval = 0;
+  window.clearInterval();
   
   window.removeEventListener('keydown', moveDodger);
   
