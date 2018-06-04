fe10b4c55fbf4bb976296fefcd9f00d9bf1a9d7e
Ian Hollander <ianholla@gmail.com>
2018-06-04 22:35:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ function createRock(x) {
 function endGame() {
   window.clearInterval(gameInterval);
   for (let i = 0; i < ROCKS.length; i++) {
-    
+    ROCKS[i].remove();
   }
   window.removeEventListener('keydown', moveDodger);
   alert('YOU LOSE!');
